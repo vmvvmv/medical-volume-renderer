4e30a7d78d088377acabf9f13421eb4d40f81492
Unknown <vmvvmv99@gmail.com>
2017-11-08 11:55:39 +0200

    Decreased brush size.

--- src/slicer.js
+++ src/slicer.js
@@ -618,7 +618,7 @@ Slicer.prototype.drawBrush = function() {
 
     for( viewport of this.viewers ) {
 
-      var brushSize = viewport.viewport.width / this.dims[0] / res_size * 6;
+      var brushSize = viewport.viewport.width / this.dims[0] / res_size * 2;
       var v = viewport.viewport;
       var deepDimension;
       var axis = viewport.axis;
